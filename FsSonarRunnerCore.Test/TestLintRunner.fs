9b53d85414318a5d8f588be4ed4573d35ff5e046
Volker Milbrandt <milbrandt@gmx.de>
2019-11-17 14:04:40 +0100

    init commit derived from https://github.com/jmecsoftware/sonar-fsharp-plugin f30024cbde16e6434585bbdb073a7b39109309f5

---
+++ FsSonarRunnerCore.Test/TestLintRunner.fs
@@ -0,0 +1,25 @@
+﻿namespace FsSonarRunnerCore.Test
+
+open System.IO
+open NUnit.Framework
+open FsSonarRunnerCore
+open System.Reflection
+
+[<TestFixture>]
+type TestLintRunner() =
+
+    let rec solutionDirectory path =
+        if Path.Combine(path, "FsSonarRunner.sln") |> File.Exists
+        then path
+        else Directory.GetParent(path).ToString() |> solutionDirectory
+    let runningPath = Assembly.GetExecutingAssembly().CodeBase.Replace("file:///", "") |> solutionDirectory
+    let fileToAnalyse = Path.Combine(runningPath, "FsSonarRunnerCore.Test", "Resources", "LintTestFile.fs")
+
+    [<Test>]
+    member this.RunLintInSource() =
+        let fileToAnalyseExists = File.Exists fileToAnalyse
+        Assume.That(fileToAnalyseExists, "File to analyze not found")
+        let lintRunner = FsLintRunner(fileToAnalyse, SonarRules(), FSharpLint.Framework.Configuration.defaultConfiguration)
+        let issues = lintRunner.ExecuteAnalysis()
+        issues |> List.iter (fun m -> printfn "%O" m)
+        Assert.That(lintRunner.ExecuteAnalysis().Length, Is.EqualTo(7))
